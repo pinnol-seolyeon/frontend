0d7e206555cdf222ba0d3d3f2898d0cd281163d4
minsikk <131092169+1224kang@users.noreply.github.com>
2025-04-19 18:37:35 +0900

    feat: ✨study page UI

--- src/pages/StudyPage.jsx
+++ src/pages/StudyPage.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Header from "../components/Header";
 import Box from "../components/study/Box";
 import tigerPencil from "../assets/tiger-pencil.png";
+import Button from "../components/Button";
 
 const Wrapper=styled.div`
     width:100%;
@@ -17,9 +18,53 @@ const Wrapper=styled.div`
 const Image=styled.img`
     width:100%; 
     height:auto;
-    object-fit:contain;
-    max-width:300px;
-`
+    object-fit:contain; /*이미지의 원본 비율을 유지 -> 이미지 전체가 보이도록 안 잘리게 */
+    max-width:380px;
+    display:block;
+    margin:0 auto; /*가로 중앙 정렬*/
+    padding:50px;
+`;
+
+const SpeechBubble=styled.div`
+    display:flex;
+    width:100%;
+    height:20%;
+    background-color:#FEF3E1;
+
+    position:relative;
+
+`;
+
+const TextBox=styled.div`
+    display:flex;
+    justify-content:center; /*가로 정렬*/
+    align-items:center; /*세로 정렬*/
+
+    width:50%;
+    margin:0 auto;
+    paddding:50px;
+    font-size: clamp(20px, 5vw, 25px); /* 최소폰트크기,뷰포트 너비 기반 크기, 최대 폰트 */
+`;
+
+
+const BubbleButton = styled.button`
+  position: absolute;
+  right: 20px;
+  bottom: 20px;
+
+  padding: 10px 16px;
+  background-color: #2774B2;
+  color: white;
+  border: none;
+  border-radius: 30px;
+  cursor: pointer;
+  border:0.2px solid black;
+
+  transition: background-color 0.3s;
+  &:hover {
+    background-color: #1b5c91;
+  }
+`;
 
 
 function StudyPage(props){
@@ -30,6 +75,10 @@ function StudyPage(props){
         <Wrapper>
             <Box>
                 <Image src={tigerPencil} alt="샘플" />
+                <SpeechBubble>
+                    <TextBox>안녕</TextBox>
+                    <BubbleButton>다음</BubbleButton>
+                </SpeechBubble>
             </Box>
         </Wrapper>
     </>
