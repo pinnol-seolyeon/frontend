61b5ac14d8a7fb9973e86db7940ba82335e07a0a
VIIXXV <jiminiyooni@sookmyung.ac.kr>
2025-04-22 17:10:27 +0900

    Fix: dashboard ui 깨짐 수정
    AppRoutes.jsx 파일생성
    App.jsx 파일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './AppRoutes'; // 👈 AppRoutes 분리해서 import
 
-import Login from '../src/pages/Login';
-import StudyPage1 from './pages/study/StudyPage1';
-import StudyPage2 from './pages/study/StudyPage2';
-import StudyLevel2 from './pages/study/StudyLevel2';
-import StudyLevel3 from './pages/study/StudyLevel3';
-import StudyLevel3_2 from './pages/study/StudyLevel3_2';
-import StudyLevel6 from './pages/study/level6/StudyLevel6'; 
-import StudyLv6_2 from './pages/study/level6/StudyLv6_2'; 
-import Question from './pages/Question';
-
-
-
-import {createGlobalStyle} from "styled-components";
-import {useState} from "react";
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
-import reset from "styled-reset"; //브라우저 기본 스타일 초기화하는 CSS코드 
-import './App.css';
-
-
-
-const GlobalStyles=createGlobalStyle`
-  ${reset}; 
-  *{
-    box-sizing:border-box;
-  }
-  
-  body{
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
-    width:100%;  
-  }
-  #root{
-    width:80%;
-
-    //border:1px solid gray;
-  }
-  
-  ::-webkit-scrollbar{
-    display:none;
-  }
-`;
-
-
-function App() {
-  const[login,setLogin]=useState(false);
+export default function App() {
+  const [login, setLogin] = useState(false);
 
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <GlobalStyles/>
-      <Routes>
-        {/* <Route path="/" element={<HomePage login={login} setLogin={setLogin}/>}></Route> */}
-        <Route path="/" element={<Dashboard />} />
-        <Route
-          path="/login"
-          element={<Login login={login} setLogin={setLogin} />}
-        ></Route>
-        <Route path="/study/1" element={<StudyPage1 login={login} setLogin={setLogin}/>}></Route>
-        <Route path="/study/2" element={<StudyPage2 login={login} setLogin={setLogin}/>}></Route>
-        <Route path="/study/level2" element={<StudyLevel2 login={login} setLogin={setLogin}/>}></Route>
-        <Route path="/study/level3" element={<StudyLevel3 login={login} setLogin={setLogin}/>}></Route>
-        <Route path="/study/level3/2" element={<StudyLevel3_2 login={login} setLogin={setLogin}/>}></Route>
-        <Route path="/study/level6" element={<StudyLevel6 login={login} setLogin={setLogin}/>}></Route>
-        <Route path="/study/level6/2" element={<StudyLv6_2 login={login} setLogin={setLogin}/>}></Route>
-        <Route path="/question" element={<Question login={login} setLogin={setLogin}/>}></Route>
-
-
-      </Routes>
+      <AppRoutes login={login} setLogin={setLogin} />
     </Router>
   );
 }
-
-export default App;
